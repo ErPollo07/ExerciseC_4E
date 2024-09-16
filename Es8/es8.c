80ae94a330f8aec65de54f04b3370e9e66b9bfe0
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-09-16 17:18:37 +0200

    Done es 8

--- Es8/es8.c
+++ Es8/es8.c
@@ -8,15 +8,37 @@ permette di effettuare il calcolo (nel dominio dei numeri reali).
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 
-    int N;
+    int N, a;
+    double sum = 0;
 
     do { 
-        
+        printf("Inserisci N: ");
+        scanf("%d", &N);
+
+        if (N < 0) {
+            printf("ATTENZIONE -> N deve essere positivo.");
+        }
     } while (N < 0);
     
+    for (int i = 0; i < N; i++) {
+        printf("Inserisci a: ");
+        scanf("%d", &a);
+
+        // If the number don't permit performing the sqrt then
+        // interrupt the program
+        if (a < 0) {
+            printf("\n\tHai inserito un numero non valido.");
+            break;
+        }
+
+        sum += sqrt(a);
+    }
+
+    printf("\n\n\tLa somma delle radici quadrate e': %f\n", sum);
 
     printf("\n\n");
     return 0;
