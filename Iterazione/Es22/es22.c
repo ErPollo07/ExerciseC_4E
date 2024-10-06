e1da5c9e1ac43ac1cfa9d8a2f34f48cbc968272a
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-10-06 15:04:49 +0200

    DONE es22

---
+++ Iterazione/Es22/es22.c
@@ -0,0 +1,26 @@
+/* 
+Dato un numero N calcolare e visualizzare tutte le coppie di numeri minori di N che
+danno per somma il numero stesso. Non considerare la proprietà commutativa.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    int n;
+     
+    printf("Inserisci n: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i <= n / 2; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i + j == n) {
+                printf("%d + %d = %d\n", i, j, n);
+            }
+        }
+    }
+
+    printf("\n\n");
+    return 0;
+} 
