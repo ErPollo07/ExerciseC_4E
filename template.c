1846375e5e2380e58d8794ba5dc0056143c5fbad
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-11-01 11:52:31 +0100

    MODIFY template.c file

--- template.c
+++ template.c
@@ -1,6 +1,13 @@
-/* 
-https://github.com/cristianogregnanin/language_C_exercises
-*/
+/**
+ * @brief 
+ * 
+ * https://github.com/cristianogregnanin/language_C_exercises
+ * 
+ * @param argc Numero di argomenti passati da riga di comando
+ * @param argv Array di stringhe passate da righe di comando.
+ * 
+ * @return La funzione restituisce 0.
+ */
 
 #include <stdio.h>
 
